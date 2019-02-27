3d673a985c8590d4cebc7ff89b6d3c875bac12f0
hansung choi <truper12@prorataart.com>
2019-02-27 18:51:12 +0900

    WEB: scoring. calc remain

--- azblr-web/app/main/service/score_service.py
+++ azblr-web/app/main/service/score_service.py
@@ -1,6 +1,7 @@
 from app.main import db
 from app.main.service.meta_service import get_inventory_types, get_fight_styles
 from itertools import product
+from copy import deepcopy
 
 def score(data):
     inventory_types = get_inventory_types()
@@ -18,7 +19,7 @@ def score(data):
         cursor.execute("select id from crawler where class_id = %s and specialization_id = %s", (class_id, specialization_id))
         crawer_ids = [r[0] for r in cursor]
         for azeritePower in item['azeritePowers']:
-            sql = "select sub_spell_name, sub_spell_id from crawler_score where crawler_id in "+str(tuple(crawer_ids))+" and spell_id = %s group by sub_spell_name"
+            sql = "select sub_spell_name, sub_spell_id from crawler_score where crawler_id in "+str(tuple(crawer_ids))+" and spell_id = %s group by sub_spell_name, sub_spell_id"
             cursor.execute(sql, azeritePower['spellId'])
             for r in cursor:
                 power = {'spellId': azeritePower['spellId'], 'spellName': azeritePower['spellName'], 'subSpellName': r[0], 'subSpellId': r[1], 'tier': azeritePower['tier']}
@@ -35,24 +36,35 @@ def score(data):
             grouped_items[item['inventoryType']] = [item,]
         
         del item['azeritePowers'] ###
-    
-    for p_items in product(*grouped_items.values()):
-        for p_powers in product(*[p['groupedPowers'].values() for p in p_items]):
-            print(p_powers)
 
+    ret = {
+        "class_id": class_id,
+        "specialization_id": specialization_id
+    }
+    scored_items = []
+    for item_comb in product(*grouped_items.values()):
+        item_set = {}
+        power_set = {}
+        for item in item_comb:
+            for power_comb in product(*item['groupedPowers'].values()):
+                item_set[item['inventoryName']] = {
+                    "id": item['id'],
+                    "name": item['name'],
+                    "inventoryType": item['inventoryType'],
+                    "inventoryName": item['inventoryName'],
+                    "selectedPower": power_comb
+                }
+                
+            for power in power_comb:
+                power_key = str(power['spellId'])+power['subSpellName']
+                if power_key in power_set:
+                    power_set[power_key] += 1
+                else:
+                    power_set[power_key] = 1
+        ##scoring
+        print(power_set)
+        scored_items.append(item_set)
 
-    # ret = {
-    #     'class_id': 3,
-    #     'specialization_id': 255,
-    #     'scores': [ {
-    #        '<fight_style_id>': [ {
-    #            '<inventory_type_id>': {
-    #                'id': 160630,
-    #                'name': '불멸의 환영의 벼슬',
-    #                'name_unique': '불멸의 환영의 벼슬 1'
-    #            }
-    #        } ]
-    #     } ]
-    # }
+    ret["scored_items"] = scored_items
 
-    return grouped_items
+    return ret
