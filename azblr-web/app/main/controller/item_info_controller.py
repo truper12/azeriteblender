6353b75e78e10c9e53e0b09791c134fd8217c679
hansung choi <truper12@prorataart.com>
2019-03-20 14:58:29 +0900

    WEB: get item info by name added

--- azblr-web/app/main/controller/item_info_controller.py
+++ azblr-web/app/main/controller/item_info_controller.py
@@ -2,7 +2,7 @@ from flask import request
 from flask_restplus import Resource
 
 from ..util.dto import ItemDto
-from ..service.item_info_service import get_item_info_with_spell
+from ..service.item_info_service import get_item_info_with_spell, get_item_info_with_spell_by_name
 
 api = ItemDto.api
 
@@ -14,3 +14,12 @@ class ItemClass(Resource):
     def get(self, item_id, class_id):
         """get a azerite item with azerite power details of the given class id"""
         return get_item_info_with_spell(item_id, class_id)
+
+@api.route('/name/<item_name>/class/<class_id>')
+@api.param('item_name', 'The Azerite Item ID')
+@api.param('class_id', 'Class ID')
+class ItemClassByName(Resource):
+    @api.doc('get a azerite item information with class power detailed')
+    def get(self, item_name, class_id):
+        """get a azerite item with azerite power details of the given class id"""
+        return get_item_info_with_spell_by_name(item_name, class_id)
--- azblr-web/app/main/service/item_info_service.py
+++ azblr-web/app/main/service/item_info_service.py
@@ -38,6 +38,53 @@ def get_item_info_with_spell(item_id, class_id):
         }
         return response_object, 500
 
+def get_item_info_with_spell_by_name(item_name, class_id):
+    inventory = get_inventory_types()
+    try:
+        try:
+            r = requests.get('https://ko.wowhead.com/search?q=%s' % item_name, allow_redirects=False, timeout=5)
+        except requests.exceptions.Timeout:
+            response_object = {
+                'status': 'fail',
+                'message': 'Unable to get azerite item information'
+            }
+            return response_object, 404    
+        if ('location' in r.headers) and ('/item=' in r.headers['location']):
+            item_id = r.headers['location'].split('=')[1].strip()
+
+            d = requests.post('https://kr.battle.net/oauth/token', data={'grant_type':'client_credentials'}, auth=(blz_client_key,blz_secret_key)).json()
+            headers = {'Authorization': 'Bearer %s' % (d['access_token'],)}
+            item = requests.get('https://kr.api.blizzard.com/wow/item/%s' % (item_id,), data={'region':'kr','locale':'ko_KR'},headers=headers).json()
+            if 'azeriteClassPowers' in item:
+                azerite_powers = item['azeriteClassPowers']
+                if class_id in azerite_powers:
+                    for azerite_power in azerite_powers[class_id]:
+                        spell = requests.get('https://kr.api.blizzard.com/wow/spell/%s' % (azerite_power['spellId'],), data={'region':'kr','locale':'ko_KR'},headers=headers).json()
+                        azerite_power['spellName'] = spell['name']
+                    data = {
+                        'id': item['id'],
+                        'name': item['name'],
+                        'inventoryType': item['inventoryType'],
+                        'inventoryName': inventory[item['inventoryType']][0],
+                        'icon': item['icon'],
+                        'slotTo': inventory[item['inventoryType']][1],
+                        'azeritePowers': azerite_powers[class_id]
+                    }
+
+                    return data
+        response_object = {
+            'status': 'fail',
+            'message': 'Unable to get azerite item information'
+        }
+        return response_object, 404
+    except Exception as e:
+        print(e)
+        response_object = {
+            'status': 'fail',
+            'message': 'Error occurs when connecting the Blizzard API Service'
+        }
+        return response_object, 500
+
 def get_spell_info(spell_id):
     try:
         d = requests.post('https://kr.battle.net/oauth/token', data={'grant_type':'client_credentials'}, auth=(blz_client_key,blz_secret_key)).json()
